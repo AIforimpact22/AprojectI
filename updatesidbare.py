e366a695e3c1a3ccf19ac3890a5552e53462fbbc
AIforimpact22 <aiforimpact22@gmail.com>
2025-04-18 12:47:03 +0300

    Update updatesidbare.py

--- updatesidbare.py
+++ updatesidbare.py
@@ -3,4 +3,8 @@ import streamlit as st
 
 def navigation() -> str:
     st.sidebar.header("📂 Navigation")
-    return st.sidebar.radio("", ["Content Manager", "Table Editor"])
+    return st.sidebar.radio(
+        label="",
+        options=["Content Manager", "Table Editor"],
+        index=0,
+    )
