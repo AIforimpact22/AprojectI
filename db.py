db9696050030570014607c1a5f4151fc9f2ebedf
AIforimpact22 <aiforimpact22@gmail.com>
2025-04-19 01:18:05 +0300

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-# db.py
-import streamlit as st
-from sqlalchemy import create_engine, text
-
-@st.cache_resource
-def get_engine():
-    conn = st.secrets["postgres"]["connection_string"]
-    return create_engine(conn, pool_pre_ping=True, isolation_level="AUTOCOMMIT")
