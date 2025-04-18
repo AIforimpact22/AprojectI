e865a9b659cac9074b6e29ace536f7480f39712f
AIforimpact22 <aiforimpact22@gmail.com>
2025-04-19 00:42:45 +0300

    Update __init__.py

--- modules_intro/__init__.py
+++ modules_intro/__init__.py
@@ -1,1 +1,10 @@
+# This makes the directory a Python package
+from .tab1 import show as show_tab1
+from .tab2 import show as show_tab2
+from .tab3 import show as show_tab3
 
+def show():
+    tab1, tab2, tab3 = st.tabs(["Welcome", "Course Instructions", "Stay Connected"])
+    with tab1: show_tab1()
+    with tab2: show_tab2()
+    with tab3: show_tab3()
